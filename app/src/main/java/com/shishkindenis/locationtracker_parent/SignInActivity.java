04672cedce184597c4b0fa471f2e885d92ee095a
HOST <sh-kin@mail.ru>
2021-02-03 19:00:40 +0300

    Test of branch "develop"

---
+++ app/src/main/java/com/shishkindenis/locationtracker_parent/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.shishkindenis.locationtracker_parent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
