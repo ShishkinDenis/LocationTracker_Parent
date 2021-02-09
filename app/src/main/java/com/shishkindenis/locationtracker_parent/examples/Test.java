607711ab7b38bdd903d21e064eb3dfef14ad983d
HOST <sh-kin@mail.ru>
2021-02-09 12:43:42 +0300

    Received location data from Firebase

---
+++ app/src/main/java/com/shishkindenis/locationtracker_parent/examples/Test.java
@@ -0,0 +1,17 @@
+package com.shishkindenis.locationtracker_parent.examples;
+
+public class Test {
+    static double x = 10;
+    double y = 20;
+
+    public static void changeX(){
+        x = 15;
+    }
+
+    public static void main(String[] args) {
+        changeX();
+        System.out.println(x);
+
+    }
+
+}
